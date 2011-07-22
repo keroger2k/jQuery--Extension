45ba1fed4f940db277e3b139deaa1ebd77c80a41
Kyle Rogers <kyle.rogers@gmail.com>
2011-07-21 23:36:02 -0500

    sort of got up/down keys working.

--- include.js
+++ include.js
@@ -3,8 +3,10 @@
   var Kyle = function (select, options) { 
 		var _select = $(select),
 		
+		debug = $('#debug-info'),
+		
 		title = select.title || "Select Option...",
-			
+	  
 		results = $('<ul/>', {
 			"class": 'results'
 		}),
@@ -16,6 +18,10 @@
 		drop = $('<div/>', {
 			"class": 'drop'
 		}),
+		
+		info = $('<div/>', {
+			"class": 'info-container'
+		}).appendTo(drop),
 
     filter = $('<input/>', {
       type: "text",
@@ -23,23 +29,34 @@
       keydown: function(e) {
         switch(e.keyCode) {
           case 9:  //Tab
+						debug.prepend("<div>Tab Selected</div>")
             toggleDrop();
             break;
           case 13: //Enter
+						debug.prepend("<div>Enter Selected</div>")
+						filter.blur();
             toggleDrop();
             break;
           case 27: //Esc
+						debug.prepend("<div>Esc Selected</div>")
             toggleDrop();
             break;
           case 38: //Up
-            break;
+            debug.prepend("<div>Up Arrow</div>")
+						highlightItem(-1);
+						break;
           case 40: //Down
+          	debug.prepend("<div>Down Arrow</div>")
+						highlightItem(1);
             break;
           default:
             //console.log(e.keyCode);
             break;
-        };
-      }
+        }
+			},
+			focus: function(e) {
+					
+			}
     }),
 		
 		search = $('<div/>', {
@@ -49,16 +66,21 @@
 		clicker = $('<a/>', {
 				href: '#',
 				"class": "clicker",
-		}).html('<span>' + title + '</span><div><b></b></div>').appendTo(container);
+		}).html('<span>' + title + '</span><div><b></b></div>').appendTo(container)
+		
+		currentPosition = -1;
 		
 		_select.hide().after(container);
 		
 		clicker.bind('click', function(e) {
+			info.html('<span>Count: # </span>')
+			filter.focus();
 			toggleDrop();		
 		});
 		
 		results.click(function(e) {
 			toggleDrop();
+			options.selected($(e.target));
 			$('span', clicker).text(e.target.innerText);
 			_select.val(e.target.value)			
 		});
@@ -79,13 +101,38 @@
 			clicker.toggleClass('with-drop');			
 		};
 		
+		var highlightItem = function(number) {
+			var items = $('li', results);
+			
+			if(currentPosition === -1 && number > 0) {
+				currentPosition += number;
+				$(items[currentPosition]).addClass('selected');
+				return;
+			}
+			
+			if(currentPosition === 0 && number < 0) { return; }
+			
+			if(currentPosition >= 0 && currentPosition < items.length - 1) {
+				$(items[currentPosition]).removeClass('selected');
+				currentPosition += number;
+				$(items[currentPosition]).addClass('selected');
+			}
+			
+			if(currentPosition === items.length - 1 && number < 0) {
+				$(items[currentPosition]).removeClass('selected');
+				currentPosition += number;
+				$(items[currentPosition]).addClass('selected');
+			}
+		};
+		
   };
 
   $.fn.extend({
     kyle: function (options) {
 
       var defaults = {
-        opt1: 20
+        opt1: 20,
+				selected: function() {}
       }
 
       var options = $.extend(defaults, options);
